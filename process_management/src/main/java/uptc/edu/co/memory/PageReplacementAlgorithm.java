55aa33731c04cbaa85d8545414d4e379f82411d5
camilin69 <camilo.merchan02@uptc.edu.co>
2025-11-20 10:09:18 -0500

    folder place not correct

--- process_management/src/main/java/uptc/edu/co/memory/PageReplacementAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package uptc.edu.co.memory;
-
-import java.util.List;
-
-public interface PageReplacementAlgorithm {
-    PageFrame selectPageToReplace(List<PageFrame> pageFrames, int currentTime);
-    String getName();
-}
